82d017407049d38b23e538871431502d6843bc8f
Naeem <ahmed.m.naeem@outlook.com>
2023-10-25 11:24:22 +0300

    - add JankStats Hilt module

---
+++ app/src/main/java/com/example/weatherapp/di/JankStatsModule.kt
@@ -0,0 +1,36 @@
+package com.example.weatherapp.di
+
+import android.app.Activity
+import android.util.Log
+import android.view.Window
+import androidx.metrics.performance.JankStats
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+object JankStatsModule {
+    @Provides
+    fun providesOnFrameListener(): JankStats.OnFrameListener {
+        return JankStats.OnFrameListener { frameData ->
+            if (frameData.isJank) {
+                Log.v("Jank", frameData.toString())
+            }
+        }
+    }
+
+    @Provides
+    fun providesWindow(activity: Activity): Window {
+        return activity.window
+    }
+
+    @Provides
+    fun providesJankStats(
+        window: Window,
+        frameListener: JankStats.OnFrameListener,
+    ): JankStats {
+        return JankStats.createAndTrack(window, frameListener)
+    }
+}
