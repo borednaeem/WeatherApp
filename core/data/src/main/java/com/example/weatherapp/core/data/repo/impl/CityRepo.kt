e29d650f4a4d593e09b92401abb7dc0d88e140cd
borednaeem <ahmed.m.naeem@outlook.com>
2023-11-05 17:24:55 +0200

    - correct type of mapper used in CityRepo

--- core/data/src/main/java/com/example/weatherapp/core/data/repo/impl/CityRepo.kt
+++ core/data/src/main/java/com/example/weatherapp/core/data/repo/impl/CityRepo.kt
@@ -1,12 +1,12 @@
 package com.example.weatherapp.core.data.repo.impl
 
-import com.example.weatherapp.core.data.mapper.ICityDataDomainMapper
+import com.example.weatherapp.core.data.mapper.ICitySearchDataDomainMapper
 import com.example.weatherapp.core.data.repo.ICityRepo
 import com.example.weatherapp.network.WeatherApiInterface
 
 class CityRepo(
     private val api: WeatherApiInterface,
-    private val cityMapper: ICityDataDomainMapper
+    private val cityMapper: ICitySearchDataDomainMapper
 ) : ICityRepo {
 
     override suspend fun searchCities(query: String): List<com.example.weatherapp.core.domain_data.model.City> {
