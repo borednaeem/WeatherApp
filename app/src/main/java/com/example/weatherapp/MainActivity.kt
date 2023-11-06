9f6d056d5015209ac128cf5f0eb6fd7ed2f3fa74
borednaeem <ahmed.m.naeem@outlook.com>
2023-11-06 16:24:47 +0200

    - create MainActivity class

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.metrics.performance.JankStats
+import androidx.metrics.performance.PerformanceMetricsState
+import com.example.weatherapp.core.ui_commons.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+}
