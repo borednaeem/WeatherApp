1f384529eefa645992bd4bb4e3c307a6080ff7dc
borednaeem <ahmed.m.naeem@outlook.com>
2024-03-02 12:25:09 +0200

    - add weather forecast api endpoint

--- network/src/main/java/com/example/weatherapp/network/weather/WeatherApiConstants.kt
+++ network/src/main/java/com/example/weatherapp/network/weather/WeatherApiConstants.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp.network.weather
 object WeatherApiConstants {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val ENDPOINT_DAY_WEATHER = "weather"
+    const val ENDPOINT_FORECAST = "forecast"
     const val PARAM_QUERY = "q"
     const val PARAM_API_KEY = "APPID"
 }
--- network/src/main/java/com/example/weatherapp/network/weather/WeatherApiInterface.kt
+++ network/src/main/java/com/example/weatherapp/network/weather/WeatherApiInterface.kt
@@ -3,6 +3,8 @@ package com.example.weatherapp.network.weather
 import com.example.weatherapp.network.BuildConfig
 import com.example.weatherapp.network.model.CitySearchDto
 import com.example.weatherapp.network.model.WeatherDto
+import com.example.weatherapp.network.model.weather.ForecastData
+import com.example.weatherapp.network.model.weather.WeatherData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,5 +13,11 @@ interface WeatherApiInterface {
     suspend fun dayWeather(
         @Query(WeatherApiConstants.PARAM_QUERY) query: String,
         @Query(WeatherApiConstants.PARAM_API_KEY) apiKey: String = BuildConfig.WEATHER_API_KEY,
-    ): WeatherDto
+    ): WeatherData
+
+    @GET(WeatherApiConstants.ENDPOINT_FORECAST)
+    suspend fun forecast(
+        @Query(WeatherApiConstants.PARAM_QUERY) query: String,
+        @Query(WeatherApiConstants.PARAM_API_KEY) apiKey: String = BuildConfig.WEATHER_API_KEY,
+    ): ForecastData
 }
