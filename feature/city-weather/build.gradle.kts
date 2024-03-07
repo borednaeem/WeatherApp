fc5a1bae13281419b16e93f70b7e21e56438fa43
borednaeem <ahmed.m.naeem@outlook.com>
2024-03-07 14:29:06 +0200

    - convert to android lib

--- feature/city-weather/build.gradle.kts
+++ feature/city-weather/build.gradle.kts
@@ -1,6 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
 }
 
