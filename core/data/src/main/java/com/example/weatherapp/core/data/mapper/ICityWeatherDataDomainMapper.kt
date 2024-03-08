3c4e37eb81321d0ba1413e60bc0efbe69d8c589f
borednaeem <ahmed.m.naeem@outlook.com>
2024-03-08 23:33:51 +0200

    - get weather data and forecast data from two different endpoints

--- core/data/src/main/java/com/example/weatherapp/core/data/mapper/ICityWeatherDataDomainMapper.kt
+++ core/data/src/main/java/com/example/weatherapp/core/data/mapper/ICityWeatherDataDomainMapper.kt
@@ -4,15 +4,21 @@ import com.example.weatherapp.core.domain_data.Constants
 import com.example.weatherapp.core.domain_data.model.CityWeather
 import com.example.weatherapp.core.domain_data.model.DayWeatherSummary
 import com.example.weatherapp.network.model.WeatherDto
+import com.example.weatherapp.network.model.weather.ForecastData
+import com.example.weatherapp.network.model.weather.WeatherData
 import com.example.weatherapp.util.DateTimeHelper
 import com.example.weatherapp.util.getDayName
 import com.example.weatherapp.util.getFormattedDate
 import com.example.weatherapp.util.getFormattedTime
+import java.util.Date
+import kotlin.math.max
+import kotlin.math.min
 
 interface ICityWeatherDataDomainMapper {
     
     fun mapFromDataToDomain(
-        weatherDto: WeatherDto
+        weatherDto: WeatherData,
+        forecastData: ForecastData
     ) : CityWeather
 
     fun mapFromDomainToData(
@@ -21,35 +27,44 @@ interface ICityWeatherDataDomainMapper {
 }
 
 class CityWeatherDataDomainMapper : ICityWeatherDataDomainMapper {
-    override fun mapFromDataToDomain(weatherDto: WeatherDto): CityWeather {
-        val localDate = DateTimeHelper.dateFromFormat(
+
+    companion object {
+        const val MAX_SUMMARY_ITEMS = 4
+    }
+    override fun mapFromDataToDomain(
+        weatherDto: WeatherData,
+        forecastData: ForecastData
+    ): CityWeather {
+        val localDate = ""/*DateTimeHelper.dateFromFormat(
             weatherDto.location?.localtime ?: "",
             Constants.API_TIME_FORMAT
-        )
+        )*/
+        val weather = weatherDto.weather.firstOrNull()
         return CityWeather(
-            cityName = weatherDto.location?.name ?: "",
-            date = localDate?.getFormattedDate() ?: "",
-            time = localDate?.getFormattedTime() ?: "",
-            summaryItems = weatherDto.forecast?.forecastday?.mapIndexed { index, forecastdayItem ->
-                val forecastDate = DateTimeHelper.dateFromFormat(
-                    forecastdayItem?.date ?: "",
-                    Constants.API_DATE_FORMAT
-                )
-                DayWeatherSummary(
-                    weatherImage = "https:${forecastdayItem?.day?.condition?.icon}",
-                    highTempInFahrenheit = forecastdayItem?.day?.maxtempF?.toFloat() ?: -1f,
-                    lowTempInFahrenheit = forecastdayItem?.day?.mintempF?.toFloat() ?: -1f,
-                    isToday = index == 0,
-                    isTomorrow = index == 1,
-                    dayName = forecastDate?.getDayName() ?: "",
-                )
-            } ?: listOf(),
-            weatherDescription = weatherDto.current?.condition?.text ?: "",
-            temperatureInFahrenheit = weatherDto.current?.tempF?.toFloat() ?: 0f,
-            temperatureImage = "https:${weatherDto.current?.condition?.icon}",
-            windSpeed = weatherDto.current?.windMph?.toFloat() ?: -1f,
-            humidityPercentage = weatherDto.current?.humidity ?: 0,
+            date = DateTimeHelper.dateFromTimestamp(weatherDto.dateTimestamp) ?: "",
+            cityName = "${weatherDto.name}, ${weatherDto.sys.country}",
+            summaryItems = mapForecastData(forecastData),
+            weatherDescription = weather?.description ?: "",
+            temperatureInFahrenheit = weatherDto.main.temp.toFloat(),
+            temperatureImage = "https://openweathermap.org/img/wn/${weather?.icon}@4x.png",
+            windSpeed = weatherDto.wind.speed.toFloat(),
+            humidityPercentage = weatherDto.main.humidity,
         )
+    }
+
+    private fun mapForecastData(forecastData: ForecastData): List<DayWeatherSummary> {
+        val summaryItems = forecastData.list.mapIndexed { index, forecastdayItem ->
+            val weather = forecastdayItem.weather.firstOrNull()
+            DayWeatherSummary(
+                weatherImage = "https://openweathermap.org/img/wn/${weather?.icon}@4x.png",
+                highTempInFahrenheit = forecastdayItem.main.temp_max.toFloat(),
+                lowTempInFahrenheit = forecastdayItem.main.temp_min.toFloat(),
+                isToday = index == 0,
+                isTomorrow = index == 1,
+                dayName = Date(forecastdayItem.dateTimestamp*1000L).getDayName(),
+            )
+        }
+        return summaryItems.subList(0, min(summaryItems.size, MAX_SUMMARY_ITEMS))
     }
 
     override fun mapFromDomainToData(weather: CityWeather): WeatherDto {
--- core/data/src/main/java/com/example/weatherapp/core/data/repo/impl/WeatherRepo.kt
+++ core/data/src/main/java/com/example/weatherapp/core/data/repo/impl/WeatherRepo.kt
@@ -4,19 +4,16 @@ package com.example.weatherapp.core.data.repo.impl
 import com.example.weatherapp.core.data.mapper.ICityWeatherDataDomainMapper
 import com.example.weatherapp.core.data.repo.IWeatherRepo
 import com.example.weatherapp.core.domain_data.model.CityWeather
-import com.example.weatherapp.network.WeatherApiInterface
-import com.example.weatherapp.network.model.WeatherDto
+import com.example.weatherapp.network.weather.WeatherApiInterface
 
 class WeatherRepo(
     private val api: WeatherApiInterface,
     private val weatherMapper: ICityWeatherDataDomainMapper
 ) : IWeatherRepo {
 
-    override suspend fun cityWeather(cityName: String, dayCount: Int): com.example.weatherapp.core.domain_data.model.CityWeather {
-        val weatherResponseData = api.dayWeather(
-            query = cityName,
-            dayCount = dayCount
-        )
-        return weatherMapper.mapFromDataToDomain(weatherResponseData)
+    override suspend fun cityWeather(cityName: String, dayCount: Int): CityWeather {
+        val weatherResponseData = api.dayWeather(query = cityName)
+        val forecastResponseData = api.forecast(query = cityName)
+        return weatherMapper.mapFromDataToDomain(weatherResponseData, forecastResponseData)
     }
 }
