b1c4e345d0d76ef5d23506f03ebbaa8667b22de1
borednaeem <ahmed.m.naeem@outlook.com>
2024-03-14 22:16:12 +0200

    - show city weather screen after splash screen

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.weatherapp
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.metrics.performance.JankStats
 import androidx.metrics.performance.PerformanceMetricsState
 import com.example.weatherapp.core.ui_commons.WeatherAppTheme
+import com.example.weatherapp.feature.city_weather.CityWeatherScreen
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -20,6 +28,19 @@ class MainActivity : ComponentActivity() {
 
         installSplashScreen()
 
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        setContent {
+            WeatherAppTheme {
+                Surface {
+                    CityWeatherScreen(
+                        viewModel = hiltViewModel()
+                    )
+                }
+            }
+        }
     }
 
     override fun onResume() {
